52dcbd229ea74ea3081689d697a17ec2c79bdf12
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-07-16 14:44:39 -0500

    started adding functions to the start button with a timer and displaying questions

--- script.js
+++ script.js
@@ -1,66 +1,112 @@
-//pop quiz 
+//pop quiz
+const startBtn = document.querySelector(".startBtn");
+//start displaying questions with questions class
+let secondsLeft = 120;
+
+let startTimer = document.querySelector(".timer");
+
 const codeQuestions = [
+  {
+      question: "What is a string?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
     {
-        question: "What is a string?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "c"
+      question: "What is an element in coding?",
+      answers: {
+        a: "Water",
+        b: "Fire",
+        c: "Brendan Eich"
       },
-      {
-        question: "What is an element in coding?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "a"
+      correctAnswer: "a"
+    },
+    {
+      question: "What does CSS stand for?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
       },
-      {
-        question: "What does CSS stand for?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "b"
+      correctAnswer: "b"
+    },
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
       },
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "c"
+      correctAnswer: "c"
+    },
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
       },
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "c"
+      correctAnswer: "c"
+    },
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
       },
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "c"
+      correctAnswer: "c"
+    },
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
       },
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
-        },
-        correctAnswer: "c"
-      },
+      correctAnswer: "c"
+    },
 ]
+
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    
+    secondsLeft--;
+    startTimer.textContent = secondsLeft + " seconds left till colorsplosion.";
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      //endGame();
+    }
+
+  }, 1000);
+}
+
+
+
+function displayQuestion() {
+  let showQuestions = document.querySelector("#showQuestion");
+  showQuestions.textContent = codeQuestions[0].question;
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+}
+
+
+
+
+function startQuiz() {
+    console.log("Hi Hello");
+    setTime();
+    displayQuestion();
+  }
+  
+
+startBtn.addEventListener("click", startQuiz);
